cd3548335a15332419f4e31d3bb67006df55fe22
markedwardmurray <markedwardmurray@gmail.com>
2015-10-05 14:53:25 -0400

    replace solution

---
+++ CategoryMath/NSNumber+FISMath.h
@@ -0,0 +1,15 @@
+//  NSNumber+FISMath.h
+
+#import <Foundation/Foundation.h>
+
+@interface NSNumber (FISMath)
+
+- (NSNumber *)numberByAddingNumber:(NSNumber *)number;
+
+- (NSNumber *)numberBySubtractingNumber:(NSNumber *)number;
+
+- (NSNumber *)numberByMultiplyingByNumber:(NSNumber *)number;
+
+- (NSNumber *)numberByDividingByNumber:(NSNumber *)number;
+
+@end
