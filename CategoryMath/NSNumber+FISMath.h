97d42d9978da4af1dcab4597772f60ce9ad4ac53
johnss518 <stacy.m.johnson@gmail.com>
2015-11-21 03:43:17 -0500

    extend NSNumber with math operations

---
+++ CategoryMath/NSNumber+FISMath.h
@@ -0,0 +1,19 @@
+//
+//  NSNumber+FISMath.h
+//  CategoryMath
+//
+//  Created by Stacy Johnson on 11/21/15.
+//  Copyright © 2015 The Flatiron School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSNumber (FISMath)
+
+
+-(NSNumber *) numberByAddingNumber:(NSNumber *)number;
+-(NSNumber *) numberBySubtractingNumber:(NSNumber *)number;
+-(NSNumber *) numberByMultiplyingByNumber:(NSNumber *)number;
+-(NSNumber *) numberByDividingByNumber:(NSNumber *)number;
+
+@end
